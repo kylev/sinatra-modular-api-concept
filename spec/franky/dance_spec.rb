51f41f9ddf760b9a50a0c83fe2cd7fb18436fc49
Kyle VanderBeek <kylev@gametime.co>
2015-08-07 12:01:21 -0700

    Go back to basics in testing, fix subject later.
    For some reason setting the subject to the parsed body causes a
    stack-level-too-deep problem. I don't yet understand why.

--- spec/franky/dance_spec.rb
+++ spec/franky/dance_spec.rb
@@ -6,17 +6,18 @@ describe Franky::Dance do
   include_context 'sinatra app'
 
   describe 'GET /dance' do
+    before { get('/dance') }
 
+    it { expect(last_response).to be_ok }
+    it { expect(json_response['step']).to eq('Waltz') }
   end
 
   describe 'GET /trip' do
     context 'with no params' do
       before { get('/trip') }
 
-      subject { last_response }
-      it { expect(true).to be_truthy }
-#      its(:last_response) { is_expected.to be_ok }
-#      its(['status']) { is_expected.to eq('error')}
+      it { expect(last_response).to be_ok }
+      it { expect(json_response['status']).to eq('error') }
     end
   end
 end
